bee925426ef02d62c63e0acd7f3ac5bffdec9c54
Zachary Chen <zacharychen0@gmail.com>
2023-02-14 18:57:23 -0800

    added manifestation templates

--- manifestation/background.py
+++ manifestation/background.py
@@ -3,9 +3,14 @@ from pytube import YouTube
 from pytube.cli import on_progress
 from moviepy.editor import VideoFileClip
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import os
 
 
-def download_background(filename="", uri=""):
+def download_background(filename="", uri="", start=0, end=0):
+    if start == 0 and end == 0:
+        print("You must specify a start and end!")
+        return
+
     Path("./manifestation/backgrounds/").mkdir(parents=True, exist_ok=True)
     # note: make sure the file name doesn't include an - in it
 
@@ -20,35 +25,35 @@ def download_background(filename="", uri=""):
 
     resolutions = [1080, 720, 480, 360]
     for res in resolutions:
-        if YouTube(uri, on_progress_callback=on_progress).streams.filter(res=f"{res}p").first() is None:
+        if YouTube(uri, on_progress_callback=on_progress).streams.filter(res=f"{res}p", progressive=True).first() is None:
             if res == resolutions[-1]:
                 print("No video resolution found!")
             continue
 
-        YouTube(uri, on_progress_callback=on_progress).streams.filter(res=f"{res}p").first().download(
-            "./manifestation/backgrounds", filename=f"{filename}"
+        YouTube(uri, on_progress_callback=on_progress).streams.filter(res=f"{res}p", progressive=True).first().download(
+            "./manifestation/backgrounds", filename=f"temp.mp4"
         )
         break
 
     print("Background video downloaded successfully! 🎉")
-
-
-def chop_background_video(start_time, end_time, video_id, background_filename):
     print("Chopping background video... ✂️")
+    chop_background_video(filename, start, end)
 
-    try:
-        ffmpeg_extract_subclip(
-            f"./manifestation/backgrounds/{background_filename}",
-            start_time,
-            end_time,
-            targetname=f"./manifestation/temp/{video_id}/background.mp4",
-        )
-    except (OSError, IOError):  # ffmpeg issue see #348
-        print("FFMPEG issue. Trying again...")
-        with VideoFileClip(f"./manifestation/backgrounds/{background_filename}") as video:
-            new = video.subclip(start_time, end_time)
-            Path(f"./manifestation/temp/{video_id}").mkdir(parents=True, exist_ok=True)
-            new.write_videofile(f"./manifestation/temp/{video_id}/background.mp4", audio_codec="aac", audio_bitrate="192k")
+
+def chop_background_video(filename, start, end):
+    # try:
+    #     ffmpeg_extract_subclip(
+    #         f"./manifestation/backgrounds/temp.mp4",
+    #         start,
+    #         end,
+    #         targetname=f"./manifestation/backgrounds/{filename}",
+    #     )
+    # except (OSError, IOError):  # ffmpeg issue see #348
+    with VideoFileClip(f"./manifestation/backgrounds/temp.mp4") as video:
+        new = video.subclip(start, end)
+        new.write_videofile(f"./manifestation/backgrounds/{filename}", audio_codec="aac", audio_bitrate="192k")
+
+    os.remove(Path(f"./manifestation/backgrounds/temp.mp4"))
     print("Background video chopped successfully! ✂️")
 
 
--- manifestation/manifestation.py
+++ manifestation/manifestation.py
@@ -1,6 +1,10 @@
 import datetime
 import shutil
 import multiprocessing
+import glob
+import os
+import random
+import pytz
 from pathlib import Path
 import manifestation.background as background
 from moviepy.editor import VideoFileClip
@@ -12,8 +16,9 @@ from PIL import ImageDraw
 from PIL import ImageFont
 from tiktok_uploader import uploadVideo
 
-
-def make_final_video():
+# For testing purposes:
+# python -c 'import manifestation.manifestation as m; m.make_final_video()'
+def make_final_video(session_id=""):
     print("Creating manifestation video 🎥")
 
     # Create temp folder
@@ -25,27 +30,61 @@ def make_final_video():
     VideoFileClip.reH = lambda clip: clip.resize(width=H)
 
     # Download and chop background clip
-    background.download_background(filename="manifestation_background_1.mp4",
-                                   uri="https://www.youtube.com/watch?v=z-vWOgYGW2g&ab_channel=SoundMystery")
-    now = datetime.datetime.now()
+    background.download_background(filename="windchimes.mp4",
+                                   uri="https://www.youtube.com/watch?v=z-vWOgYGW2g&ab_channel=SoundMystery",
+                                   start=30,
+                                   end=36)
+    background.download_background(filename="wheat.mp4",
+                                   uri="https://www.youtube.com/watch?v=uklsxwTnrAo&ab_channel=AECIPlantHealth",
+                                   start=10,
+                                   end=17)
+    background.download_background(filename="ocean.mp4",
+                                   uri="https://www.youtube.com/watch?v=lDWoInMdjJQ&ab_channel=MyTranquilitee",
+                                   start=708,
+                                   end=715)
+    background.download_background(filename="nebula.mp4",
+                                   uri="https://www.youtube.com/watch?v=ZPxfHGbVwj8&ab_channel=TeunvanderZalm",
+                                   start=260,
+                                   end=268)
+    background.download_background(filename="bedroom.mp4",
+                                   uri="https://www.youtube.com/watch?v=xg1gNlxto2M&ab_channel=Choolutter",
+                                   start=9840,
+                                   end=9846)
+    now = datetime.datetime.now(tz=pytz.utc)
+    now = now.astimezone(pytz.timezone('US/Pacific'))
     video_id = f"{now.year}_{now.month}_{now.day}_{now.hour}h_{now.minute}m"
-    background.chop_background_video(30, 36, video_id, "manifestation_background_1.mp4")
+
+    background_vids = []
+    for file in glob.glob('./manifestation/backgrounds/*'):
+        background_vids.append(os.path.basename(file))
+    random_vid = random.choice(background_vids)
+
     background_clip = (
-        VideoFileClip(f"./manifestation/temp/{video_id}/background.mp4")
+        VideoFileClip(f"./manifestation/backgrounds/{random_vid}")
             # .without_audio()
             .resize(height=H)
             .crop(x1=1166.6, y1=0, x2=2246.6, y2=1920)
     )
 
     # Create subtitle text
-    font = ImageFont.truetype("fonts/SFPro/SFPRODISPLAYMEDIUM.OTF", 60)
-    template = f"CONGRATULATIONS!!!!!! A permanent position with a higher salary is coming " \
-               f"to you within days and you're about to receive a huge amount of wealth " \
-               f"within the next 12 hours. Interact in 3 ways to claim!"
+    def suffix(d):
+        return 'th' if 11 <= d <= 13 else {1: 'st', 2: 'nd', 3: 'rd'}.get(d % 10, 'th')
 
+    def custom_strftime(format, t):
+        return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
+
+    templates = {}
+    templates[0] = f"CONGRATULATIONS!!!!!! A permanent position with a higher salary is coming to you within days and you're about to receive a huge amount of wealth within the next 12 hours. Interact in 3 ways to claim!"
+    templates[1] = f"If you are seeing this video on the {custom_strftime('{S}', now)} or {custom_strftime('{S}', now.replace(day=now.day+1))} of {now.strftime('%B')}, you are about to receive a LARGE amount of wealth in 7-8 hours. Don't skip, interact in at least 3 ways to claim or at least save it for the drafts. Karma is real."
+    templates[2] = f"If you are seeing this video on the {custom_strftime('{S}', now)} or {custom_strftime('{S}', now.replace(day=now.day+1))} of {now.strftime('%B')}, you are about to receive the best news of your life in 3-5 hours after watching this. Use this sound (could be private) and interact in at least 3 ways to claim."
+    templates[3] = f"This is your last chance. It doesn't matter if you've skipped all the other sounds, but if you skip this one, it's over. You will live the best life possible, have a lot of fun, money, and a long lasting relationship with your crush. Don't risk skipping. Interact in 3 ways to claim."
+    templates[4] = f"If you are seeing this video on the {custom_strftime('{S}', now)} or {custom_strftime('{S}', now.replace(day=now.day+1))} of {now.strftime('%B')}, this video is meant for you. The love of your life will find their way to you in the next 1-2 days, and they will fall in love with you. Interact in 3 ways to claim."
+
+    font = ImageFont.truetype("fonts/SFPro/SFPRODISPLAYMEDIUM.OTF", 60)
     lines = []
     cur = ""
-    maxLength = 650
+    maxLength = 700
+    template = random.choice(list(templates.values()))
     for i, char in enumerate(template):
         cur += char
         next_space = template.find(" ", i + 1)
@@ -67,13 +106,11 @@ def make_final_video():
     for line in lines:
         w, h = draw.textsize(line, font=font)
         curX = (1080 - w) // 2
-        draw.text((curX-3, curY-3), line, fill="black", font=font)
-        draw.text((curX+3, curY-3), line, fill="black", font=font)
-        draw.text((curX-3, curY+3), line, fill="black", font=font)
-        draw.text((curX+3, curY+3), line, fill="black", font=font)
+        draw.text((curX, curY), line, fill="black", font=font, stroke_width=4)
         draw.text((curX, curY), line, fill="white", font=font)
         curY += 70
 
+    Path(f"./manifestation/temp/{video_id}").mkdir(parents=True, exist_ok=True)
     img.save(f"./manifestation/temp/{video_id}/text.png")
 
     # Generate Video
@@ -100,8 +137,7 @@ def make_final_video():
     shutil.rmtree(Path(f"./manifestation/temp/{video_id}"))
 
     # Post to TikTok
-    session_id = ""
     file = f"./manifestation/results/{video_id}.mp4"
     title = "Manifest wealth 💸 good things are coming"
     tags = ["manifestation", "fyp", "spirituality"]
-    uploadVideo(session_id, file, title, tags, verbose=True)
+    uploadVideo(session_id, file, title, tags)
